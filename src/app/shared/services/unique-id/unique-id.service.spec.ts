cb7c1aab3fad3c77177c10c6e1654c6d8d614bbf
Thiago Teófilo <thiago.teofilo@shift.com.br>
2022-05-17 12:06:32 -0300

    adicionando validacao no metodo de gerar ID

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,9 +1,20 @@
 import { UniqueIdService } from "./unique-id.service";
 
-describe('UniqueIdService', () => {
-  it('#generateUniqueIdWithPrefix  should generate id when called with prefix', () => {
+describe(UniqueIdService.name, () => {
+  let prefixMethod = `#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}`;
+
+  it(`${prefixMethod} should generate id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
-    expect(id).toContain('app-');
-  })
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`${prefixMethod} should not generate duplicate IDs when called with multiple times`, () => {
+    const service = new UniqueIdService();
+    const ids = new Set();
+    for(let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
 });
